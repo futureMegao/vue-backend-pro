a14ff0e239e09c6e1464405d1e4e532bc7d57316
futureMegao <30866301+futureMegao@users.noreply.github.com>
2017-10-01 16:20:19 +0800

    update

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,22 +1,54 @@
 <template>
   <div id="app">
 
-    <ul class="menu-sub">
-      <li>
-        <router-link to="/permissionsList">权限列表</router-link>
-      </li>
-      <li>
-        <router-link to="/user">用户列表</router-link>
-      </li>
-    </ul>
+    <div class="top-banner">
+
+    </div>
+    <div>
+      <div class="left-bar-menu">
+        <el-menu router default-active="2" theme="dark" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose">
+          <el-menu-item index="2"><i class="el-icon-star-off"></i>简述</el-menu-item>
+          <el-submenu index="1">
+            <template slot="title"><i class="el-icon-setting"></i>权限</template>
+            <el-menu-item-group>
+              <template slot="title">权限列表</template>
+              <el-menu-item index="permissionsList">选项1</el-menu-item>
+              <el-menu-item index="user">选项2</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="用户列表">
+              <el-menu-item index="1-3">选项3</el-menu-item>
+            </el-menu-item-group>
+            <el-submenu index="1-4">
+              <template slot="title">选项4</template>
+              <el-menu-item index="1-4-1">选项1</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+
+          <el-menu-item index="3"><i class="el-icon-setting"></i>导航三</el-menu-item>
+        </el-menu>
+
+      </div>
+      <div class="breadcrumb"></div>
+    </div>
 
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  export default {
+    name : 'app',
+    data(){
+      return {}
+    },
+    methods : {
+      handleOpen(key, keyPath) {
+        console.log(key, keyPath);
+      },
+      handleClose(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
+  }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -4,6 +4,20 @@ import Vue from 'vue'
 import App from './components/App'
 import router from './router'
 
+import './css/index.css'
+import './css/custome.css'
+
+
+import { Menu, Submenu,MenuItem ,MenuItemGroup } from 'element-ui'
+
+Vue.component(Menu.name, Menu)
+Vue.component(Submenu.name, Submenu)
+Vue.component(MenuItem.name, MenuItem)
+Vue.component(MenuItemGroup.name, MenuItemGroup)
+
+
+
+
 import store from './store/index.js'
 
 /*import axios from 'axios'
